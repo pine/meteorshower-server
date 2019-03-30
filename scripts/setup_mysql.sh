e6cec91cd4966ca5696575c718988d64991bc6e3
Pine Mizune <pinemz@gmail.com>
2019-03-30 19:00:12 +0900

    fix: fix mysql hostname

--- scripts/setup_mysql.sh
+++ scripts/setup_mysql.sh
@@ -7,22 +7,22 @@ set -eu -o pipefail
 
 sql='DROP DATABASE IF EXISTS `meteorshower`;'
 echo "$sql"
-echo "$sql" | mysql -u root
+echo "$sql" | mysql -h 127.0.0.1 -u root
 
 sql='DROP DATABASE IF EXISTS `meteorshower_test`;'
 echo "$sql"
-echo "$sql" | mysql -u root
+echo "$sql" | mysql -h 127.0.0.1 -u root
 
 
 # ----- CREATE DATABASE -----------------------------------
 
 sql='CREATE DATABASE `meteorshower`;'
 printf "\n$sql\n"
-printf "\n$sql\n" | mysql -u root
+printf "\n$sql\n" | mysql -h 127.0.0.1 -u root
 
 sql='CREATE DATABASE `meteorshower_test`;'
 echo "$sql"
-echo "$sql" | mysql -u root
+echo "$sql" | mysql -h 127.0.0.1 -u root
 
 
 # ----- CREATE TABLE : auth -------------------------------
@@ -30,12 +30,12 @@ echo "$sql" | mysql -u root
 sql=$(cat "./lib/auth/src/test/resources/drop_tables.sql")
 sql="\n"'USE `meteorshower`;'"\n\n$sql\n"
 printf "$sql"
-printf "$sql" | mysql -u root
+printf "$sql" | mysql -h 127.0.0.1 -u root
 
 sql=$(cat "./lib/auth/src/test/resources/create_tables.sql")
 sql="\n"'USE `meteorshower`;'"\n\n$sql\n"
 printf "$sql"
-printf "$sql" | mysql -u root
+printf "$sql" | mysql -h 127.0.0.1 -u root
 
 
 # ----- CREATE TABLE : setting ----------------------------
@@ -43,9 +43,9 @@ printf "$sql" | mysql -u root
 sql=$(cat "./lib/setting/src/test/resources/drop_tables.sql")
 sql="\n"'USE `meteorshower`;'"\n\n$sql\n"
 printf "$sql"
-printf "$sql" | mysql -u root
+printf "$sql" | mysql -h 127.0.0.1 -u root
 
 sql=$(cat "./lib/setting/src/test/resources/create_tables.sql")
 sql="\n"'USE `meteorshower`;'"\n\n$sql\n"
 printf "$sql"
-printf "$sql" | mysql -u root
+printf "$sql" | mysql -h 127.0.0.1 -u root
