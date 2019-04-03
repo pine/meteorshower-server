b121385ba4bb1f15c8ae524e019714e598831718
Pine Mizune <pinemz@gmail.com>
2019-04-03 14:42:14 +0900

    fix: return not found

--- app/src/main/kotlin/moe/pine/meteorshower/controllers/HealthCheckController.kt
+++ app/src/main/kotlin/moe/pine/meteorshower/controllers/HealthCheckController.kt
@@ -1,10 +1,12 @@
 package moe.pine.meteorshower.controllers;
 
 import moe.pine.meteorshower.properties.AppProperties
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
+import javax.servlet.http.HttpServletResponse
 
 
 @Controller
@@ -12,8 +14,14 @@ class HealthCheckController(
     val appProperties: AppProperties
 ) {
     @GetMapping("/")
-    fun home(): String {
-        return "redirect:${appProperties.siteUrl}"
+    fun home(response: HttpServletResponse) {
+        val redirectUrl = appProperties.siteUrl
+        if (redirectUrl.isBlank()) {
+            response.sendError(HttpStatus.NOT_FOUND.value())
+            return
+        }
+
+        response.sendRedirect(redirectUrl)
     }
 
     @GetMapping("/health", produces = [MediaType.TEXT_PLAIN_VALUE])
