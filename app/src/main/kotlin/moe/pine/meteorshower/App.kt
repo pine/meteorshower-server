d2e7a476f7bc5855ce838d947a66ba161c6c4aae
Pine Mizune <pinemz@gmail.com>
2019-05-18 18:07:05 +0900

    fix: show debug log

--- app/src/main/kotlin/moe/pine/meteorshower/App.kt
+++ app/src/main/kotlin/moe/pine/meteorshower/App.kt
@@ -2,6 +2,7 @@ package moe.pine.meteorshower
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.net.URI
 
 @SpringBootApplication
 class App
@@ -10,6 +11,12 @@ fun main(args: Array<String>) {
     val port = System.getenv("PORT") ?: ""
     if (port.isNotEmpty()) {
         System.setProperty("server.port", port)
+    }
+
+    val redisUrl = System.getenv("REDIS_URL") ?: ""
+    if (redisUrl.isNotEmpty()) {
+        val parsedUri = URI.create(redisUrl)
+        println(parsedUri)
     }
 
     runApplication<App>(*args)
