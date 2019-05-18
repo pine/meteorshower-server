a9a903f5fcf2e863c7df794793d0f3bef98d6fc7
Pine Mizune <pinemz@gmail.com>
2019-05-18 18:21:48 +0900

    fix: set params from environment varialble

--- app/src/main/kotlin/moe/pine/meteorshower/App.kt
+++ app/src/main/kotlin/moe/pine/meteorshower/App.kt
@@ -1,11 +1,17 @@
 package moe.pine.meteorshower
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import java.net.URI
 
 @SpringBootApplication
-class App
+class App {
+    companion object {
+        @JvmField
+        val LOGGER = LoggerFactory.getLogger(App::class.java)
+    }
+}
 
 fun main(args: Array<String>) {
     val port = System.getenv("PORT") ?: ""
@@ -16,6 +22,9 @@ fun main(args: Array<String>) {
     val redisUrl = System.getenv("REDIS_URL") ?: ""
     if (redisUrl.isNotEmpty()) {
         val parsedUri = URI.create(redisUrl)
+        App.LOGGER.debug("spring.redis.host={}", parsedUri.host)
+        App.LOGGER.debug("spring.redis.password={}", parsedUri.authority)
+        App.LOGGER.debug("spring.redis.port={}", parsedUri.port)
         System.setProperty("spring.redis.host", parsedUri.host)
         System.setProperty("spring.redis.password", parsedUri.authority)
         System.setProperty("spring.redis.port", parsedUri.port.toString())
