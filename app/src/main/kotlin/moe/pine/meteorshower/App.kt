497add574426a3eaf08937048f9137f7553f5a2f
Pine Mizune <pinemz@gmail.com>
2019-05-18 23:39:52 +0900

    feat: use library

--- app/src/main/kotlin/moe/pine/meteorshower/App.kt
+++ app/src/main/kotlin/moe/pine/meteorshower/App.kt
@@ -1,10 +1,10 @@
 package moe.pine.meteorshower
 
+import moe.pine.heroku.addons.HerokuRedis
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import java.net.URI
 
 @SpringBootApplication
 class App {
@@ -20,16 +20,10 @@ fun main(args: Array<String>) {
         System.setProperty("server.port", port)
     }
 
-    val redisUrl = System.getenv("REDIS_URL") ?: ""
-    if (redisUrl.isNotEmpty()) {
-        val parsedUri = URI.create(redisUrl)
-        val password = parsedUri.userInfo.split(":", limit = 2)[1]
-        App.LOGGER.debug("spring.redis.host={}", parsedUri.host)
-        App.LOGGER.debug("spring.redis.password={}", password)
-        App.LOGGER.debug("spring.redis.port={}", parsedUri.port)
-        System.setProperty("spring.redis.host", parsedUri.host)
-        System.setProperty("spring.redis.password", password)
-        System.setProperty("spring.redis.port", parsedUri.port.toString())
+    if (HerokuRedis.isDetected()) {
+        System.setProperty("spring.redis.host", HerokuRedis.getHost())
+        System.setProperty("spring.redis.password", HerokuRedis.getPassword())
+        System.setProperty("spring.redis.port", Integer.toString(HerokuRedis.getPort()))
     }
 
     runApplication<App>(*args)
